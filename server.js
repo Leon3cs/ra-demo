40a02f396c866afc6a8be1ade82c2ae6069a53fc
Rafael <rafael21_psp@hotmail.com>
2026-08-19 23:57:56 -0300

    initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const routes = require("./src/routes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
